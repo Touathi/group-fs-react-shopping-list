641a6074aacbb89ebaa53714058577cac44a04f6
Touathi Vang <Touathi@outlook.com>
2023-06-01 11:20:05 -0500

    add database

--- database.sql
+++ database.sql
@@ -1,2 +1,18 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- fs-react-shopping 
+
+
+CREATE TABLE "food" (
+id SERIAL PRIMARY KEY,
+name VARCHAR(80) NOT NULL,
+quantity FLOAT NOT NULL,
+unit VARCHAR (25) NOT NULL,
+purchased BOOLEAN DEFAULT FALSE);
+
+
+INSERT INTO "food" ("name", "quantity", "unit")
+VALUES ('Apples', '5', 'lbs');
+
+SELECT * FROM "food";
